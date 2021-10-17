528a36d9cf728ea1954bc2e9b169a5c56781b7f0
Bridger Herman <herma582@umn.edu>
2021-10-16 22:33:42 -0500

    Add colors to events

--- quickstart.py
+++ quickstart.py
@@ -19,6 +19,21 @@ TIME_TRACKING_CAL_ID = 'data/calendar_id.txt'
 # CSV file for time tracking
 ATIMELOGGER_REPORT = 'data/report-16-10-2021.csv'
 
+# Activity type => color ID mapping
+COLOR_MAP = {
+    'Research ABR': '10',
+    'Research Other': '6',
+    'Research Physicalization': '4',
+    'Dissertation': '4',
+    'Meeting': '11',
+
+    'Teaching Prep': '3',
+    'Teaching': '3',
+    'Grading': '3',
+
+    'Volunteer': '9'
+}
+
 
 def load_report(report_csv):
     '''
@@ -42,7 +57,7 @@ def load_report(report_csv):
             entries.append(activity)
     return entries
 
-def add_calendar_evt(service, calendar_id, name: str, from_dt: datetime.datetime, to_dt: datetime.datetime):
+def add_calendar_evt(service, calendar_id, name: str, from_dt: datetime.datetime, to_dt: datetime.datetime, color_id: str=None):
     '''
     Create an event on the Time Tracking calendar with a specified start and end time
     '''
@@ -55,6 +70,8 @@ def add_calendar_evt(service, calendar_id, name: str, from_dt: datetime.datetime
             'dateTime': to_dt.isoformat()
         }
     }
+    if color_id is not None:
+        body['colorId'] = color_id
     return service.events().insert(calendarId=calendar_id, body=body).execute()
 
 def main():
@@ -81,6 +98,9 @@ def main():
 
     service = build('calendar', 'v3', credentials=creds)
 
+    colors_result = service.colors().get().execute()
+    event_colors = colors_result['event']
+
     # Load name of calendar
     with open(TIME_TRACKING_CAL_ID) as fin:
         calendar_id = fin.read()
@@ -103,12 +123,21 @@ def main():
     ).execute()
     events = events_result.get('items', [])
 
+    # Option to delete all events in this time range and return
+    # proceed = input('Are you sure you want to delete all events from {} to {}? (y/n): '.format(beginning.isoformat(), end.isoformat())).lower() == 'y'
+    # if proceed:
+    #     for evt in events:
+    #         service.events().delete(calendarId=calendar_id, eventId=evt['id']).execute()
+    #         print('Deleted event `{}` (was id {})'.format(evt['summary'], evt['id']))
+    #     return
+
     # Create an easy lookup to determine if an event is a duplicate:
     # {"summary1": [{'start': <datetime>, 'end': <datetime>}], "summary2":  ...}
     existing = {}
     for evt in events:
         summary = evt['summary']
         new_rec = {}
+        new_rec['id'] = evt['id']
         for field in ['start', 'end']:
             new_rec[field] = datetime.datetime.fromisoformat(evt[field]['dateTime'])
 
@@ -123,12 +152,21 @@ def main():
 
         # First, check if already in calendar
         if act_type in existing:
-            if start in map(lambda a: a['start'], existing[act_type]) and end in map(lambda a: a['end'], existing[act_type]):
-                print('Activity `{}` starting {} already exists, skipping'.format(act_type, start.isoformat()))
-                continue
+            try:
+                start_match = [a['start'] for a in existing[act_type]].index(start)
+            except ValueError:
+                start_match = None
+
+            if start_match is not None:
+                delete = input('Activity `{}` starting {} already exists. Delete? (y/n): '.format(act_type, start.isoformat())).lower() == 'y'
+                if delete:
+                    service.events().delete(calendarId=calendar_id, eventId=existing[act_type][start_match]['id']).execute()
+                    print('  -> Deleted event `{}` with id {}'.format(act_type, existing[act_type][start_match]['id']))
+                else:
+                    continue
 
         print('Adding calendar event for {}, starting {}'.format(act_type, start.isoformat()))
-        result = add_calendar_evt(service, calendar_id, act_type, start, end)
+        result = add_calendar_evt(service, calendar_id, act_type, start, end, color_id=COLOR_MAP.get(act_type, None))
         print('  -> Added event `{}`, starting {}'.format(result['summary'], result['start']['dateTime']))
 
 if __name__ == '__main__':
